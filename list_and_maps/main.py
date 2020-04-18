431c47cc5123ba8e965599db990d6904925e2d2f
Cristiano <neivacristiano@yahoo.com.br>
2020-04-18 18:06:31 -0300

    lists and tuples

---
+++ list_and_maps/main.py
@@ -0,0 +1,26 @@
+x = [1, 2 , 3]
+# z = (1, 2) not mutable
+print(x[-3])
+
+print(x + [4, 5])
+
+print(x[1:2])
+
+y = [1, 5, 67, 2, 4]
+
+print(sorted(y))
+
+print([[1, 2]][0][1])
+
+l = ['cristiano', 10.1, 1982]
+
+l.extend(['pop', 10])
+l.append(5)
+
+del(l[0])
+
+print('eae men'.split())
+
+l = y[:] # copy
+
+l.index('cristiano')
