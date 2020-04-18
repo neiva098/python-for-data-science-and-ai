419d4400cb4db2e6058bf55a283c6b63c1d12fa9
Cristiano <neivacristiano@yahoo.com.br>
2020-04-18 17:33:42 -0300

    strings

---
+++ strings/main.py
@@ -0,0 +1,22 @@
+x = 'michael e top dms'
+
+print(x[-2])
+
+print(x[::2])
+
+print(x[0:5:3])
+
+print(len(x))
+
+print(3*x)
+
+# x[0] = 'h'
+# strings sao imutaveis
+
+print(r'top\ndms')
+
+print('barbarabarbara'.replace('barbara', 'cristiano'))
+
+print(x.find('o'))
+
+print('uppercase'.upper())
